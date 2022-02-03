6f3203284d2797c5179c3a03291840ace24923e9
Rikuta Furuta <rikuta0811@g.ecc.u-tokyo.ac.jp>
2022-02-03 23:30:56 +0900

    edit: setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,25 @@
-from distutils.core import setup, Extension
-from tabnanny import verbose
-from Cython.Build import cythonize
+from setuptools import find_packages, setup, Extension
+import os
 import glob
-import setuptools
 
 
-### Compile with Cython begins ###
+
 sources = glob.glob("factorizer/*.pyx")+glob.glob("factorizer/*.cpp")
+if "factorizer.cpp" in sources:
+    sources.remove("factorizer.cpp")
 ext = Extension("factorizer", 
     sources = sources,
-    include_dirs = ['./factorizer'],
     language = "c++",
-    extra_compile_args = ["-v", "-std=c++11"],
+    extra_compile_args = ["-v", "-std=c++11", "-Wall"],
     extra_link_args = ["-std=c++11"]
 )
-setup(
-    name = "factorizer", 
-    ext_modules = cythonize([ext],language_level=3)
-)
 
-### Compile with Cython ends ###
-
-
-
-### For downlode through GitHub begins ###
 with open("README.md", "r") as fp:
     long_description = fp.read()
 with open("requirements.txt", "r") as fp:
     install_requires = fp.read().splitlines()
 
-setuptools.setup(
+setup(
     name = "factorizer",
     version = "0.0.1",
     author = "Fulltea",
@@ -37,9 +27,12 @@ setuptools.setup(
     long_description = long_description,
     long_description_content_type="text/markdown",
     url = "https://github.com/FullteaOfEEIC/factorizer",
-    packages = setuptools.find_packages(),
+    packages = find_packages(),
     install_requires = install_requires,
-    python_requires = '>=3.7'
+    python_requires = '>=3.7',
+    ext_modules = [ext]
 )
 
-### For downlode through GitHub ends ###
+if os.path.exists(os.path.join("factorizer", "factorizer.cpp")):
+    os.remove(os.path.join("factorizer", "factorizer.cpp"))
+
