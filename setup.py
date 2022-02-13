8d30813ca55e49a5bd43c50c5acb8ef448eccc50
Rikuta Furuta <rikuta@furutan.com>
2022-02-13 16:32:18 +0900

    fix: bug?

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import glob
 sources = [] 
 sources += glob.glob("src/*.cpp")
 sources += glob.glob("src/*.pyx")
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
     
 ext = Extension("factorizer", 
     sources = sources,
@@ -13,14 +15,14 @@ ext = Extension("factorizer",
     extra_link_args = ["-std=c++14"]
 )
 
-with open("README.md", "r") as fp:
+with open(os.path.join(root_dir, 'README.md'), "r") as fp:
     long_description = fp.read()
-with open("requirements.txt", "r") as fp:
+with open(os.path.join(root_dir, 'requirements.txt'), "r") as fp:
     install_requires = fp.read().splitlines()
 
 setup(
     name = "factorizer",
-    version = "0.9.2",
+    version = "0.9.3",
     author = "Fulltea",
     author_email = "rikuta@furutan.com",
     long_description = long_description,
