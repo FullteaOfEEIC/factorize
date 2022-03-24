70b6660d36f636063c176623c5d2b5ee949e0211
Rikuta Furuta <rikuta@furutan.com>
2022-03-24 10:12:11 +0900

    add src/utils.cpp: jacobi_symbol

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/multiprecision/cpp_int.hpp>
 #include <cmath>
+#include <cassert>
 
 using namespace std;
 using Bint = boost::multiprecision::cpp_int;
@@ -33,4 +34,45 @@ Bint euclidean_gcd(Bint a, Bint b){
         r = a%b;
     }
     return b;
+}
+
+Bint jacobi_symbol(Bint a, Bint p){
+    //returns (a/p)
+    //https://en.wikipedia.org/wiki/Jacobi_symbol
+    assert(p%2==1);
+    a = a%p; // (12) p43 [Wada 2001]
+    if(a==1){ // (11) p43 [Wada 2001]
+        return ONE;
+    }
+    else if(a==-1){ // (14) p43 [Wada 2001]
+        if(a%4==1){
+            return ONE;
+        }
+        else{
+            return MINUS_ONE;
+        }
+    }
+    else if(a==2){ // (15) p43 [Wada 2001]// (15) p43 [Wada 2001]
+        switch(a%8){
+            case 1:
+            case 7:
+                return ONE;
+            case 3:
+            case 5:
+                return MINUS_ONE;
+        }
+    }
+
+    if(a%2==0){ // (13) p43 [Wada 2001]
+        return jacobi_symbol(2, p) * jacobi_symbol(a/2, p);
+    }
+    else{ // (16) p43 [Wada 2001]
+        assert(euclidean_gcd(a,p)==1);
+        if(a%4==1 or p%4==1){
+            return jacobi_symbol(p, a);
+        }
+        else{
+            return MINUS_ONE * jacobi_symbol(p, a);
+        }
+    }
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -11,4 +11,11 @@ unsigned long ilog(Bint n);
 
 Bint euclidean_gcd(Bint a, Bint b);
 
+Bint jacobi_symbol(Bint a, Bint p);
+
+#define ZERO Bint("0")
+#define ONE Bint("1")
+#define MINUS_ONE Bint("-1")
+
+
 #endif // UTILS_HPP_FACTORIZER_4e17c669_cf51_4c34_8ac1_ae3306060740
